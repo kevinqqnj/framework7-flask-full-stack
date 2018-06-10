025d100d3026fc06b34d1d4f26bd720124459d37
kevinqqnj <ezhqing@gmail.com>
2018-06-10 16:05:57 +0800

    phonegap

---
+++ auto.py
@@ -0,0 +1,102 @@
+# encoding: utf-8
+# auto build (phonegap+Framework7+vue+Flask), then git push to heroku
+# http://psutil.readthedocs.io/
+# python git.py -h: means push to heroku
+
+import os, sys, time, re
+import psutil
+from subprocess import PIPE
+
+GIT_REMOTE = 'heroku'
+INDEX_HTML = 'app/templates/index.html'
+NODE_DIR = "C:/Program Files/nodejs/"
+if not os.path.exists(NODE_DIR):
+  NODE_DIR = "C:/Program Files (x86)/nodejs/"
+NODE = NODE_DIR + "node.exe"
+PHONEGAP = NODE_DIR + "node_modules/phonegap/bin/phonegap.js"
+if not os.path.exists(PHONEGAP):
+  PHONEGAP = 'C:/Users/{}/AppData/Roaming/npm/'.format(os.environ.get('username')) + 'node_modules/phonegap/bin/phonegap.js'
+
+def find_procs_by_name(name):
+  "Return a list of processes matching 'name'."
+  ls = []
+  for proc in psutil.process_iter():
+      try:
+          pinfo = proc.as_dict(attrs=['pid', 'name', 'username'])
+          if pinfo['name'].lower().find(name)>-1:
+              print(pinfo)
+              ls.append(pinfo['pid'])
+      except psutil.NoSuchProcess:
+          print(proc.as_dict)
+  return ls
+
+################################################################
+#                      主程序开始                              #
+################################################################
+if __name__ == '__main__':
+  usg = """
+  -? 显示本信息
+  -h git push remote "heroku", not "origin"
+  -m git commit "msg"，用双引号表示, "msg" must be last argv
+  """
+  if '-?' in sys.argv:
+    print(usg)
+    exit()
+  if not '-h' in sys.argv:
+    GIT_REMOTE = 'origin'
+  GIT_MSG = '"auto"'
+  if '-m' in sys.argv:
+    GIT_MSG = '"{}"'.format(sys.argv[-1])
+    # for aa in sys.argv:
+
+  with psutil.Popen(['git', 'status'], stdout=PIPE) as proc:
+    rsp = (proc.stdout.read().decode('ascii'))
+    is_changed = "no changes added to commit"
+    is_clear = "nothing to commit, working directory clean"
+    if rsp.find(is_changed):
+      print(rsp)
+    else:
+      input(is_clear)
+      exit()
+
+  print('closing current node.exe...')
+  for p in find_procs_by_name('node'):
+    psutil.Process(p).terminate()
+  print('======== run phonegap build...')
+
+  with psutil.Popen([NODE, PHONEGAP, 'build', '--release'], stdout=PIPE) as proc:
+  # with psutil.Popen(["ipconfig"], stdout=PIPE) as proc:
+    print(proc.stdout.read().decode('ascii'))   # 中文系统用 .decode('gbk'), 英文系统：'ascii'
+  # os.remove('app/static/main.css')
+  # os.remove('app/static/main.js')
+
+  print('======== copy main.js, styles.css to app/static')
+  # 删除旧的js/css
+  olds = os.listdir('app/static')
+  for f in olds:
+  	  if re.search('main[\d]{3,}.', f): os.remove('app/static/'+f)
+  stamp = str(time.time()).split('.')[0]
+  # 移动+改名
+  os.rename('www/main.js', 'app/static/main{}.js'.format(stamp))
+  os.rename('www/styles.css', 'app/static/main{}.css'.format(stamp))
+
+  print('======== update main.js&main.css in index.html')
+  with open(INDEX_HTML, 'r') as f:
+    html = f.read()
+  html = re.sub('main[\d]{3,}.js', 'main{}.js'.format(stamp), html)
+  html = re.sub('main[\d]{3,}.css', 'main{}.css'.format(stamp), html)
+  html = re.sub('value="[\d]{3,}"', 'value="{stamp}"'.format(stamp=stamp), html)	# 转义{} -> {{}}
+  with open(INDEX_HTML, 'w') as f:
+    f.write(html)
+
+  print('======== git push to Heroku...')
+  with psutil.Popen(['git', 'add', '.'], stdout=PIPE) as proc:
+    print(proc.stdout.read().decode('ascii'))
+  with psutil.Popen(['git', 'commit', '-m', GIT_MSG], stdout=PIPE) as proc:
+    print(proc.stdout.read().decode('ascii'))
+  # print(proc.communicate())
+  with psutil.Popen(['git', 'push', GIT_REMOTE, 'master'], stdout=PIPE) as proc:
+    print(proc.stdout.read().decode('ascii'))
+
+  # print('Done!')
+  input('Done!')
---
+++ src/main.js
@@ -0,0 +1,53 @@
+// Import Vue
+import Vue from 'vue'
+
+// Import Framework7
+import Framework7 from 'framework7/dist/framework7.esm.bundle.js';
+
+// Import Framework7 Vue
+import Framework7Vue from 'framework7-vue/dist/framework7-vue.esm.bundle.js';
+
+// Import F7 Style
+import Framework7CSS from 'framework7/dist/css/framework7.css'
+
+// Import F7 iOS Icons
+import Framework7Icons from 'framework7-icons/css/framework7-icons.css'
+
+// Import Fontawesome Theme Styles
+import FontAwesome from 'font-awesome/css/font-awesome.css'
+
+// Import App Custom Styles
+// import AppStyles from './assets/sass/main.scss'
+
+// Import App Component
+import app from './main.vue'
+
+// Import Routes
+import routes from './routes.js'
+
+// Import Vuex Storage
+import store from './assets/vuex/storage.js'
+
+// Install Plugin
+Vue.use(Framework7Vue, Framework7);
+
+let theme = 'auto';
+if (document.location.search.indexOf('theme=') >= 0) {
+  theme = document.location.search.split('theme=')[1].split('&')[0];
+}
+
+// Init Vue App
+export default new Vue({
+  // Root Element
+  el: '#app',
+  store,
+  render: c => c('app'),
+  components: {
+    app,
+  },
+  framework7: {
+    id: 'io.framework7.testapp',
+    theme, // md or ios
+  },
+  routes,
+});
